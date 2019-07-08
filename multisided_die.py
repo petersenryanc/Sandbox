dc7dbc47ba1ec2b3d3d103730d6da8b0f00372a8
petersenryanc <ryan.petersen@my.jcu.edu.au>
2019-07-08 11:38:58 +1000

    Add user input to multisided_die

--- multisided_die.py
+++ multisided_die.py
@@ -6,10 +6,23 @@ Create and roll n-sided dice
 from MSDie import MSDie
 
 
+# def main():
+#     die_1 = MSDie(12)
+#     die_1.roll()
+#     print(die_1.get_value())
+
 def main():
-    die_1 = MSDie(12)
-    die_1.roll()
-    print(die_1.get_value())
+    try:
+        n_sides = int(input("Enter number of sides: "))
+        while n_sides <= 0:
+            print("Number of sides must be greater than zero.")
+            n_sides = input("Enter number of sides: ")
+        die = MSDie(n_sides)
+        die.roll()
+        print("Die roll results in {}".format(die.get_value()))
+    except ValueError:
+        print("Must be a number greater than zero")
+        main()
 
 
 main()
